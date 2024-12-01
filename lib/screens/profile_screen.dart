af0618622f68418eeb40bc3e78785bbd39a34f35
Leander-Haddal <122444622+Leander-Haddal@users.noreply.github.com>
2024-12-01 14:24:56 +0100

    style and fire update to profile page

--- lib/screens/profile_screen.dart
+++ lib/screens/profile_screen.dart
@@ -1,10 +1,43 @@
 import 'package:flutter/material.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
-import 'auth.dart'; // Import Auth functionality if needed
-import 'settings_screen.dart'; // Import SettingsScreen
+import 'auth.dart';
+import 'settings_screen.dart';
+import 'package:mobile_app_group5/themes/app_theme.dart';
 
 class ProfileScreen extends StatelessWidget {
   const ProfileScreen({super.key});
+
+  Future<Map<String, dynamic>> _getCurrentUserData() async {
+    final currentUser = FirebaseAuth.instance.currentUser;
+
+    if (currentUser == null) {
+      return {
+        'username': '[[DEV]]',
+        'email': 'Not Available',
+        'image_url': null,
+      };
+    }
+
+    final userDoc = await FirebaseFirestore.instance
+        .collection('users')
+        .doc(currentUser.uid)
+        .get();
+
+    if (userDoc.exists) {
+      return {
+        'username': userDoc.data()?['username'] ?? '[[DEV]]',
+        'email': userDoc.data()?['email'] ?? 'Not Available',
+        'image_url': userDoc.data()?['image_url'],
+      };
+    } else {
+      return {
+        'username': '[[DEV]]',
+        'email': 'Not Available',
+        'image_url': null,
+      };
+    }
+  }
 
   Future<void> _logout(BuildContext context) async {
     await FirebaseAuth.instance.signOut();
@@ -17,43 +50,116 @@ class ProfileScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Profile'),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            const CircleAvatar(
-              radius: 50,
-            ),
-            const SizedBox(height: 16),
-            const Text(
-              'Username',
-              style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
-            ),
-            const SizedBox(height: 24),
-            ElevatedButton(
-              onPressed: () async {
-                await _logout(context); // Call logout function
-              },
-              style: ElevatedButton.styleFrom(
-                backgroundColor: Colors.red,
-              ),
-              child: const Text('Logout'),
-            ),
-            const SizedBox(height: 16),
-            ElevatedButton(
-              onPressed: () {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(
-                      builder: (context) => const SettingsScreen()),
-                );
-              },
-              child: const Text('Settings'),
-            ),
-          ],
+        title: const Text(
+          'Profile',
+          style: TextStyle(
+            fontWeight: FontWeight.bold,
+            fontSize: 22,
+            color: Colors.white,
+          ),
         ),
+        centerTitle: true,
+        backgroundColor: Theme.of(context).colorScheme.secondary,
+        elevation: 0,
+      ),
+      body: FutureBuilder<Map<String, dynamic>>(
+        future: _getCurrentUserData(),
+        builder: (context, snapshot) {
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return const Center(
+              child: CircularProgressIndicator(),
+            );
+          }
+          if (snapshot.hasError) {
+            return const Center(
+              child: Text('Error fetching user data'),
+            );
+          }
+
+          final userData = snapshot.data!;
+          final username = userData['username'];
+          final email = userData['email'];
+          final imageUrl = userData['image_url'];
+
+          return Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              Expanded(
+                flex: 1,
+                child: Container(
+                  color: Theme.of(context).colorScheme.primary,
+                  padding: const EdgeInsets.symmetric(horizontal: 16),
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.end,
+                    children: [
+                      CircleAvatar(
+                        radius: 50,
+                        backgroundColor: Theme.of(context).appBarTheme.backgroundColor,
+                        backgroundImage:
+                            imageUrl != null ? NetworkImage(imageUrl) : null,
+                      ),
+                      const SizedBox(height: 16),
+                      Text(
+                        username,
+                        style: const TextStyle(
+                          fontSize: 20,
+                          fontWeight: FontWeight.bold,
+                          color: Colors.black,
+                        ),
+                      ),
+                      const SizedBox(height: 8),
+                      Text(
+                        email,
+                        style: const TextStyle(
+                          fontSize: 16,
+                          color: Colors.grey,
+                        ),
+                      ),
+                      const SizedBox(height: 16),
+                    ],
+                  ),
+                ),
+              ),
+              Expanded(
+                flex: 1,
+                child: Container(
+                  color: Theme.of(context).scaffoldBackgroundColor,
+                  padding: const EdgeInsets.symmetric(vertical: 40, horizontal: 16),
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      ElevatedButton(
+                        onPressed: () {
+                          Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                              builder: (context) => const SettingsScreen(),
+                            ),
+                          );
+                        },
+                        style: ElevatedButton.styleFrom(
+                          backgroundColor: AppTheme.blueColor,
+                          fixedSize: const Size(150, 50),
+                        ),
+                        child: const Text('Settings'),
+                      ),
+                      ElevatedButton(
+                        onPressed: () async {
+                          await _logout(context);
+                        },
+                        style: ElevatedButton.styleFrom(
+                          backgroundColor: Colors.red,
+                          fixedSize: const Size(150, 50),
+                        ),
+                        child: const Text('Logout'),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            ],
+          );
+        },
       ),
     );
   }
